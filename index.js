b8bb413530f0573f4e10edb465b1012b4a4bad46
samuelrharahap <47378787+samuelrharahap@users.noreply.github.com>
2024-01-20 08:17:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.post('/v1/product-stocks/bulk-update', (req, res) => {
   res.status(200).send('OK')
 })
 
+// Route to handle POST requests
+app.get('/shopify/callback', (req, res) => {
+  console.log('Received shopify request:', req)
+
+  // Send 'OK' response
+  res.status(200).send('OK')
+})
+
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
